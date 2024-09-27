9522a2f5e41afc7b238b582271207567035a76cf
BGMarinho <bgmarinho96@gmail.com>
2024-09-26 23:29:08 -0300

    chore (all together): Putting it all together.

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-const handleClick = () => {
-  postFormData();
+const button = document.getElementById("form-button");
+button.addEventListener("click", (e) => {
+  feedbackMessage(0);
+  e.preventDefault();
+});
+
+const feedbackMessage = (numReturned) => {
+  const feedback = document.getElementById("feedback-message");
+  if (numReturned === 0) {
+    feedback.style.display = "block";
+    feedback.innerText = "Não possui doença cardíaca";
+    setTimeout(() => {
+      feedback.style.display = "none";
+    }, 2000);
+  }
+  feedback.style.display = "block";
+  feedback.innerText = "Possui doença cardíaca";
+  setTimeout(() => {
+    feedback.style.display = "none";
+  }, 2000);
 };
 
-// tratar o feedback
-const postFormData = async () => {
-  const form = document.getElementsByTagName("form");
-  const formData = new FormData(form);
-  const url = "";
-  fetch(url, { method: "POST", body: formData })
-    .then((res) => {
-      if (res.status >= 400) {
-        return response.text().then((error) => {
-          const errorJson = JSON.parse(error);
-          throw new Error(errorJson.error);
-        });
-      }
-    })
-    .catch((error) => {
-      presentError(error);
-    });
-};
+// const postFormData = async () => {
+//   const form = document.getElementsByTagName("form");
+//   const formData = new FormData(form);
+//   const url = "";
+//   fetch(url, { method: "POST", body: formData })
+//     .then((res) => {
+//       if (res.status >= 400) {
+//         return response.text().then((error) => {
+//           const errorJson = JSON.parse(error);
+//           throw new Error(errorJson.error);
+//         });
+//       }
+//     })
+//     .catch((error) => {
+//       presentError(error);
+//     });
+// };
